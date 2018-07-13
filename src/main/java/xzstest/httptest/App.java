9b3fa539524ffd4e4dd5ed3db2455fcadbf6af7e
xiaofengyangliu <970360490@qq.com>
2018-07-13 11:46:12 +0800

    新增项目

---
+++ src/main/java/xzstest/httptest/App.java
@@ -0,0 +1,13 @@
+package xzstest.httptest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
